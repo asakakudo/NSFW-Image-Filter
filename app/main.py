432b8b2b5dde00e7122a386f06d673c8c3f3f4d5
C.C Arcides <ptpradnya2@gmail.com>
2025-10-02 19:34:04 +0800

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-# File: app/main.py
-
-from app.ui.main_window import MainWindow
-
-if __name__ == "__main__":
-    # Membuat instance dari jendela utama kita
-    app = MainWindow()
-    # Menjalankan event loop aplikasi (membuat jendela tetap terbuka)
-    app.mainloop()
